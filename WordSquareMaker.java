514c581057039a8326eef0863e78426c7c79e456
Jeremy <jeremycustomer@gmail.com>
2022-01-23 15:27:31 -0500

    Added thread names and timestamps

--- WordSquareMaker.java
+++ WordSquareMaker.java
@@ -1,5 +1,7 @@
+
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 
@@ -7,13 +9,24 @@ import java.util.Set;
 public class WordSquareMaker implements Runnable {
     private static final int WORD_LENGTH = 5;
     private WordList wl;
-
+    ThreadLocal<String> threadName = new ThreadLocal<String>();
+    Random rand = new Random();
+    int counter = 0;
+    private Node tree = new Node();
+    private long timeStamp = System.currentTimeMillis();
     public WordSquareMaker() {
         wl = new WordList("griddle5.txt");
         loadTree();
     }
-    private Node tree = new Node();
 
+    private long getTS() {
+        return System.currentTimeMillis()-timeStamp;
+    }
+    private String generateThreadName() {
+        String[] adjectives = {"funny", "silly", "happy", "crazy", "bouncy", "stealthy", "wise","giant","tiny","magnificent","magnanimous","angry","clever", "weird", "funny","sad","fierce","gentle"};
+        String[] nouns = {"fairy", "monk","tiger","eagle","dragon","snake","bull","coyote","elf","orc","wizard","witch","hawk","shark","bunny","wolf","bear","warrior"};
+        return adjectives[rand.nextInt(adjectives.length)] + "-" + nouns[rand.nextInt(nouns.length)];
+    }
     public ArrayList<String> matchPattern(String pattern) {
         ArrayList<String> matchList = new ArrayList<String>();
         tree.getMatches(pattern, tree, matchList,"");
@@ -25,9 +38,10 @@ public class WordSquareMaker implements Runnable {
             tree.insertWord(w, tree);
          }
     }
-    public void dumpGrid(String grid) {
-        System.out.println("wordsquare.."+grid);
+    public synchronized void dumpGrid(String grid) {
         //tm = time.time()-ts
+        counter ++;
+        System.out.println("Wordsquare "+counter+" found by thread: "+ threadName.get() + " Timestamp - "+ getTS());
         for (int i=0; i < WORD_LENGTH; i++) {
             System.out.println(grid.substring(i*WORD_LENGTH, WORD_LENGTH*(i+1)));
         }
@@ -85,21 +99,30 @@ public class WordSquareMaker implements Runnable {
         WordSquareMaker wt = new WordSquareMaker();
         wt.loadTree();
         System.out.println("Tree Loaded..");
-        String[] mList =  {"shark"};
-        for (String seed : mList) {
-            System.out.println(" seeding with - "+seed);
-            wt.tryNextWord(seed+"????????????????????", 1, new HashSet <String>());
-        }
-        // System.out.println("Starting Threads..");
-        // for (int t = 0; t < 4; t++) {
-        //     Thread th = new Thread(wt);
-        //     th.start();
+        // String[] mList =  {"shark"};
+        // for (String seed : mList) {
+        //     System.out.println(" seeding with - "+seed);
+        //     wt.tryNextWord(seed+"????????????????????", 1, new HashSet <String>());
         // }
+        System.out.println("Starting Threads..");
+        for (int t = 0; t < 4; t++) {
+            Thread th = new Thread(wt);
+            th.start();
+        }
      }
     @Override
     public void run() {
-        for (int x = 0; x < 200; x++) {
-            System.out.println("word "+ x + ") "+ wl.getNextWord());
+        String sillyName = generateThreadName();
+        System.out.println("creating thread.. "+ sillyName);
+        threadName.set(sillyName);
+        String seed = wl.getNextWord();
+        while (seed.length()>0) {
+            System.out.println("\nSeeding with " + seed);
+            HashSet<String> usedSet = new HashSet<String>();
+            usedSet.add(seed);
+            tryNextWord(seed+"????????????????????", 1,usedSet);
+            seed = wl.getNextWord();
         }
+        System.out.println("Thread done. ");
     }
 }
